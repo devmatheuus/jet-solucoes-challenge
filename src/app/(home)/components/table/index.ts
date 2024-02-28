ef212d72d6f18968ed122612187374fdca1cdee3
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-28 18:30:55 -0300

    feat: create TableHead component

--- src/app/(home)/components/table/index.ts
+++ src/app/(home)/components/table/index.ts
@@ -1,5 +1,7 @@
+import { TableHead } from "./TableHead";
 import { TableRoot } from "./TableRoot";
 
 export const Table = {
   Root: TableRoot,
+  Head: TableHead,
 };
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,9 @@ const Home: React.FC = () => {
   return (
     <main className="m-auto flex w-screen max-w-screen-xl flex-1 justify-center p-4 ">
       <Table.Root>
-        <></>
+        <Table.Head className="w-full bg-gray-50">
+          <></>
+        </Table.Head>
       </Table.Root>
     </main>
   );
