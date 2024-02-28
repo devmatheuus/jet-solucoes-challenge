f90c298e09e27bfb91c55e9bec1a6282840c6e0d
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-28 17:24:27 -0300

    chore: initial setup

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { Table } from "./components/table/table";
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <main className="px- flex flex-1 justify-center">
+      <Table />
+    </main>
+  );
+};
+
+export default Home;
