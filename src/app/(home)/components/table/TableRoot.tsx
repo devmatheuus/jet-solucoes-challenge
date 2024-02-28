f1486cd9b63b3044351308b701bad8497332b9f2
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-28 18:31:36 -0300

    refactor: create generic interface to Children Type

--- src/app/(home)/components/table/TableRoot.tsx
+++ src/app/(home)/components/table/TableRoot.tsx
@@ -1,8 +1,7 @@
+import { IGenericChildren } from "@/interfaces/Children";
 import React from "react";
 
-interface ITableRootProps {
-  children: React.ReactNode;
-}
+interface ITableRootProps extends IGenericChildren {}
 
 export const TableRoot: React.FC<ITableRootProps> = ({ children }) => {
   return <table className="w-full text-left shadow">{children}</table>;
