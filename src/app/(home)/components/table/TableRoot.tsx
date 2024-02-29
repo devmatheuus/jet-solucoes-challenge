8960c7802415a986a1b60d4c58ac924bf8093b84
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-29 00:33:30 -0300

    style: fix radius

--- src/app/(home)/components/table/TableRoot.tsx
+++ src/app/(home)/components/table/TableRoot.tsx
@@ -5,6 +5,8 @@ interface ITableRootProps extends IGenericChildren {}
 
 export const TableRoot: React.FC<ITableRootProps> = ({ children }) => {
   return (
-    <table className="w-full rounded-xl text-left shadow">{children}</table>
+    <table className="w-full rounded-bl-xl rounded-br-xl text-left shadow">
+      {children}
+    </table>
   );
 };
