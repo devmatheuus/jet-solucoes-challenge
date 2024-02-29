6738e0641d7daadf6e2befd70a024a24d1e220bb
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-28 21:39:35 -0300

    style: add border-radius to component

--- src/app/(home)/components/table/TableRoot.tsx
+++ src/app/(home)/components/table/TableRoot.tsx
@@ -4,5 +4,7 @@ import React from "react";
 interface ITableRootProps extends IGenericChildren {}
 
 export const TableRoot: React.FC<ITableRootProps> = ({ children }) => {
-  return <table className="w-full text-left shadow">{children}</table>;
+  return (
+    <table className="w-full rounded-xl text-left shadow">{children}</table>
+  );
 };
