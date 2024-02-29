f7156bb604c2ab8210c7b1702edac7a57aefba08
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-29 00:34:18 -0300

    fix: ts error

--- src/hooks/useTableHeaderTitle.tsx
+++ src/hooks/useTableHeaderTitle.tsx
@@ -5,8 +5,14 @@ type ITableHeaderTitles =
   | "rating"
   | "actions";
 
-export const useTableHeaderTitle = (): {headerTitles: ITableHeaderTitles[]} => {
-  const headerTitles: ITableHeaderTitles[] = ["car", "next reservation", "status", "rating", "actions"];
+export const useTableHeaderTitle = () => {
+  const headerTitles: ITableHeaderTitles[] = [
+    "car",
+    "next reservation",
+    "status",
+    "rating",
+    "actions",
+  ];
 
   return { headerTitles };
 };
