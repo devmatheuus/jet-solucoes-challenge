db1a67fd78513479002518b82bbaf5bc0745160b
Matheus Silva <matheus_lsilva@hotmail.com>
2024-02-28 22:33:10 -0300

    refactor: removing header title from data structure

--- src/hooks/useCarData.tsx
+++ src/hooks/useCarData.tsx
@@ -2,65 +2,62 @@ import { StaticImageData } from "next/image";
 import carImage from "../assets/images/webp/car.webp";
 
 type ICarData = {
-  tableHeaderTitle:
-    | "car"
-    | "next reservation"
-    | "status"
-    | "rating"
-    | "actions";
   img: StaticImageData;
   name: string;
   startReservationDate: Date;
   endReservationDate: Date;
-  status: "available" | "unavailable";
+  status: "Available" | "Unavailable";
   rating: string;
 };
 
 export const useCarData = (): Record<string, ICarData[]> => {
   const cars: ICarData[] = [
     {
-      tableHeaderTitle: "car",
       img: carImage,
       name: "Mini Cooper 2020",
       startReservationDate: new Date("2024-07-6"),
       endReservationDate: new Date("2024-07-012"),
-      status: "available",
+      status: "Available",
       rating: "4.5",
     },
     {
-      tableHeaderTitle: "next reservation",
       img: carImage,
       name: "Mini Cooper 2020",
       startReservationDate: new Date("2024-07-6"),
       endReservationDate: new Date("2024-07-012"),
-      status: "available",
+      status: "Available",
       rating: "4.5",
     },
     {
-      tableHeaderTitle: "status",
       img: carImage,
       name: "Mini Cooper 2020",
       startReservationDate: new Date("2024-07-6"),
       endReservationDate: new Date("2024-07-012"),
-      status: "available",
+      status: "Available",
       rating: "4.5",
     },
     {
-      tableHeaderTitle: "rating",
       img: carImage,
       name: "Mini Cooper 2020",
       startReservationDate: new Date("2024-07-6"),
       endReservationDate: new Date("2024-07-012"),
-      status: "available",
+      status: "Available",
       rating: "4.5",
     },
     {
-      tableHeaderTitle: "actions",
       img: carImage,
       name: "Mini Cooper 2020",
       startReservationDate: new Date("2024-07-6"),
       endReservationDate: new Date("2024-07-012"),
-      status: "available",
+      status: "Unavailable",
+      rating: "4.5",
+    },
+    {
+      img: carImage,
+      name: "Mini Cooper 2020",
+      startReservationDate: new Date("2024-07-6"),
+      endReservationDate: new Date("2024-07-012"),
+      status: "Unavailable",
       rating: "4.5",
     },
   ];
